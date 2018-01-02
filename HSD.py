b1ea206445eb15919de53703c9c2bf28b398be44
Carlos Pascual <carlos.rpg@gmail.com>
2018-01-02 19:12:11 +0100

    Move high speed data adquisition functions to HSD module

---
+++ HSD.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pandas as pd
+
+
+def process_high_speed_data_file(data_file):
+
+    # Read the tsv data file excluding the first rows
+    high_speed_data = pd.read_csv(data_file, skiprows=4, sep='\t')
+
+    # Center the stroke data so the max and min limits are equidistant to
+    # zero
+    limits_average = (high_speed_data.loc[:, 'HSD Stroke'].max() +
+                      high_speed_data.loc[:, 'HSD Stroke'].min()) / 2
+
+    high_speed_data.loc[:, 'HSD Stroke'] -= limits_average
+
+    # Calculate the movement direction for each data row
+    HSD_stroke = high_speed_data.loc[:, 'HSD Stroke'].append(
+        pd.Series(np.nan, index=[high_speed_data.shape[0]]))
+
+    HSD_stroke_minus_1 = pd.Series(np.nan, index=[-1]).append(
+        high_speed_data.loc[:, 'HSD Stroke'])
+
+    HSD_stroke_minus_1.index += 1
+    high_speed_data['HSD Direction'] = (
+        HSD_stroke - HSD_stroke_minus_1).apply(np.sign)
+    high_speed_data.dropna(inplace=True)
+    high_speed_data.index -= 1
+
+    # Calculate the cycle every data row belongs to
+    class Tracker:
+        cycle = 0
+        initial_sign = high_speed_data.get_value(0, 'HSD Direction')
+        former_sign = -initial_sign
+
+    def assign_cycle(sign):
+
+        if Tracker.initial_sign == sign and Tracker.former_sign != sign:
+            Tracker.cycle += 1
+
+        Tracker.former_sign = sign
+        return Tracker.cycle
+
+    high_speed_data['HSD Cycle'] = high_speed_data.loc[:, 'HSD Direction'].apply(assign_cycle)
+
+    # Filter out data that isn't located around the centre
+    max_filter_limit = high_speed_data.loc[:, 'HSD Stroke'].max() * 0.05
+    min_filter_limit = high_speed_data.loc[:, 'HSD Stroke'].min() * 0.05
+
+    central_values = ((high_speed_data.loc[:, 'HSD Stroke'] <= max_filter_limit) &
+                      (high_speed_data.loc[:, 'HSD Stroke'] >= min_filter_limit))
+
+    filtered_high_speed_data = high_speed_data.loc[central_values]
+
+    # Forces in absolute value
+    filtered_high_speed_data.loc[:, 'HSD Friction'].apply(np.abs)
+
+    # Group data by cycle and average values for each group
+    averaged_high_speed_data = filtered_high_speed_data.groupby(
+        'HSD Cycle').mean()
+
+    # Save data
+    averaged_high_speed_data.drop('HSD Direction', axis=1, inplace=True)
+    averaged_high_speed_data.reset_index(inplace=True)
+    data_file_name = data_file[:-4]
+    averaged_high_speed_data.to_csv(f'{data_file_name}.csv', index=False)
--- TE38.py
+++ TE38.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import numpy as np
-import pandas as pd
 import os.path
+import HSD
 
 
 def extract_file_name(file_path, extract_file_extension=True):
@@ -22,7 +21,7 @@ def extract_file_name(file_path, extract_file_extension=True):
         all return 'bar.txt' with the default keyword, otherwise returns 'bar'
     """
     file_name_with_extension = os.path.split(file_path)[-1]
-    
+
     if extract_file_extension:
         return file_name_with_extension
     else:
@@ -67,70 +66,8 @@ def extract_high_speed_file_name(text_line):
     return text_line[initial_index:final_index]
 
 
-def process_high_speed_data_file(data_file):
-    
-    # Read the tsv data file excluding the first rows
-    high_speed_data = pd.read_csv(data_file, skiprows=4, sep='\t')
-    
-    # Center the stroke data so the max and min limits are equidistant to
-    # zero
-    limits_average = (high_speed_data.loc[:, 'HSD Stroke'].max() + 
-                      high_speed_data.loc[:, 'HSD Stroke'].min()) / 2
-    
-    high_speed_data.loc[:, 'HSD Stroke'] -= limits_average
-    
-    # Calculate the movement direction for each data row
-    HSD_stroke = high_speed_data.loc[:, 'HSD Stroke'].append(
-                            pd.Series(np.nan, index=[high_speed_data.shape[0]]))
-
-    HSD_stroke_minus_1 = pd.Series(np.nan, index=[-1]).append(
-                            high_speed_data.loc[:, 'HSD Stroke'])
-    
-    HSD_stroke_minus_1.index += 1
-    high_speed_data['HSD Direction'] = (HSD_stroke - HSD_stroke_minus_1).apply(np.sign)
-    high_speed_data.dropna(inplace=True)
-    high_speed_data.index -= 1
-
-    # Calculate the cycle every data row belongs to
-    class Tracker:
-        cycle = 0
-        initial_sign = high_speed_data.get_value(0, 'HSD Direction')
-        former_sign = -initial_sign
-
-    def assign_cycle(sign):
-    
-        if Tracker.initial_sign == sign and Tracker.former_sign != sign:
-            Tracker.cycle += 1
-    
-        Tracker.former_sign = sign
-        return Tracker.cycle
-    
-    high_speed_data['HSD Cycle'] = high_speed_data.loc[:, 'HSD Direction'].apply(assign_cycle)
-
-    # Filter out data that isn't located around the centre 
-    max_filter_limit = high_speed_data.loc[:, 'HSD Stroke'].max() * 0.05
-    min_filter_limit = high_speed_data.loc[:, 'HSD Stroke'].min() * 0.05
-    
-    central_values = ((high_speed_data.loc[:, 'HSD Stroke'] <= max_filter_limit) & 
-                      (high_speed_data.loc[:, 'HSD Stroke'] >= min_filter_limit))
-    
-    filtered_high_speed_data = high_speed_data.loc[central_values]
-    
-    # Forces in absolute value
-    filtered_high_speed_data.loc[:, 'HSD Friction'].apply(np.abs)
-    
-    # Group data by cycle and average values for each group
-    averaged_high_speed_data = filtered_high_speed_data.groupby('HSD Cycle').mean()
-
-    # Save data
-    averaged_high_speed_data.drop('HSD Direction', axis=1, inplace=True)
-    averaged_high_speed_data.reset_index(inplace=True)
-    data_file_name = data_file[:-4]
-    averaged_high_speed_data.to_csv(f'{data_file_name}.csv', index=False)
-
-
 def process_test_files(main_test_file_path):
-    
+
     main_test_file_name = extract_file_name(main_test_file_path)
     with open(main_test_file_name) as origin_file:
         # Skip the initial rows until the real data begins
@@ -139,12 +76,12 @@ def process_test_files(main_test_file_path):
                 break
             else:
                 continue
-        
+
         file_name = extract_file_name(main_test_file_name, False)
         table_header = convert_data_row_to_csv_format(origin_file.readline())
         # Add the two new columns that will be extracted from the fast data
         # files
-        table_header = ','.join([table_header, 
+        table_header = ','.join([table_header,
                                  'HSD Friction Force (N)',
                                  'HSD Friction Coeff\n'])
 
@@ -153,12 +90,11 @@ def process_test_files(main_test_file_path):
             for line in origin_file:
                 if line.startswith('\t'):
                     new_data_row = convert_data_row_to_csv_format(line)
-                    # Commas mark the space for the two new columns, 
+                    # Commas mark the space for the two new columns,
                     # followed by a new line char
                     final_file.write(''.join([new_data_row, ',,\n']))
                 elif line.startswith('Fast data in'):
                     high_speed_file_name = extract_high_speed_file_name(line)
-                    process_high_speed_data_file(high_speed_file_name)
+                    HSD.process_high_speed_data_file(high_speed_file_name)
                 else:
                     break
-
